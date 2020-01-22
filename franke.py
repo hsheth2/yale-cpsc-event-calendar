1aa954e358c2ee2726bb006d6fd5d370a81a14a7
Harshal Sheth <hsheth2@gmail.com>
2020-01-22 14:47:37 -0500

    Add franke

---
+++ franke.py
@@ -0,0 +1,85 @@
+import datetime
+from pprint import pprint
+import pytz
+import requests
+from bs4 import BeautifulSoup
+
+from scraping_common import parse_event_urls_from_feed
+from generate_ics import generate_ics
+
+domain = 'https://frankeprogram.yale.edu'
+upcoming_events_url = f'{domain}/calendar'
+
+
+def franke_upcoming_events():
+    return parse_event_urls_from_feed(domain, upcoming_events_url)
+
+
+def franke_event_info(event_url):
+    page = requests.get(event_url)
+
+    soup = BeautifulSoup(page.text, 'html5lib')
+    soup.prettify()
+
+    content_region = soup.find(id='region-content')
+
+    title_region = content_region.find(id='page-title')
+    title = title_region.get_text().strip()
+
+    content = content_region.find('article', class_='node-event')
+    content = content.find(class_='content')
+
+    time_region = content.find(class_='field-name-field-event-time')
+    time_element = time_region.find(class_='date-display-single')
+    if time_element.find(class_='date-display-range'):
+        time_element = time_element.find(class_='date-display-range')
+        time_element = time_element.find(class_='date-display-start')
+    time_raw = time_element.get('content')
+    time_aware = datetime.datetime.strptime(time_raw, "%Y-%m-%dT%H:%M:%S%z")
+    time_real = time_aware.astimezone(tz=pytz.utc).replace(tzinfo=None)
+
+    try:
+        location_region = content.find(class_='field-name-field-location')
+        location_region = location_region.find(class_='location')
+        location_region.find('span', class_='map-icon').extract()
+        location_name = location_region.find(class_='fn').get_text().strip()
+    except:
+        location_name = "TBA"
+
+    description_region = content.find(class_='field-name-body')
+    description_texts = description_region.findAll(text=True)
+    description = '\n'.join(text.strip() for text in description_texts)
+    # description = ''.join(description_texts)
+    description += '\n\n' + event_url
+
+    return {
+        'title': title,
+        'time': time_real,
+        'location': location_name,
+        'description': description,
+        'url': event_url,
+    }
+
+
+def franke_generate(filename):
+    with open(filename, 'wb') as f:
+        print('Generating Franke Program Calendar')
+
+        events = []
+        urls = franke_upcoming_events()
+        for url in urls:
+            event_info = franke_event_info(url)
+            print(f"Event: {event_info['title']}")
+            events.append(event_info)
+
+        cal = generate_ics('Yale Franke Program', 'Yale Franke Program in Science and the Humanities Event Calendar', events)
+        f.write(cal)
+        print()
+
+
+if __name__ == '__main__':
+    results = franke_upcoming_events()
+    pprint(results)
+
+    event_info = franke_event_info(results[1])
+    pprint(event_info)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from cpsc import cpsc_generate
 from yins import yins_generate
+from franke import franke_generate
 
 if __name__ == '__main__':
     cpsc_generate('calendars/cpsc_events.ics')
     yins_generate('calendars/yins_events.ics')
+    franke_generate('calendars/franke_events.ics')
 
